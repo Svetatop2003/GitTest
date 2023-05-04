85a407185fdb16d21b094a546728c6c086ff0512
Svetlana The Best <122528234+Svetatop2003@users.noreply.github.com>
2023-05-04 13:34:29 +0300

    pr10
    load to reposit

---
+++ GitTest/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GitTest;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Всем привет");
+    }
+}
